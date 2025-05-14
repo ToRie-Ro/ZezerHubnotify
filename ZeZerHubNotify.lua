4bf91f3bd0b1a3f8dd6be2315e08ed13120a5a72
RealCambo <gamermakara40@gmail.com>
2025-05-14 16:25:13 +0700

    Add files via upload

---
+++ ZeZerHubNotify.lua
@@ -0,0 +1,9 @@
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "ZeZer Hub",
+    Text = "The script has been loaded.",
+    Button1 = "Yes",
+    Button2 = "No",
+    Duration = 10
+})
