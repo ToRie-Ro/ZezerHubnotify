c7915f82ea1bc2284c2522855851b15b4a89820e
RealCambo <gamermakara40@gmail.com>
2025-05-15 11:56:57 +0700

    Update ZeZerHubGwAGard.lua

--- ZeZerHubGwAGard.lua
+++ ZeZerHubGwAGard.lua
@@ -1,5 +1,5 @@
-local ZeZerHubCh = loadstring(game:HttpGet("https://raw.githubusercontent.com/ToRie-Ro/ZezerHubnotify/refs/heads/main/Checking.lua"))()
-local ZeZerHubNotify = loadstring(game:HttpGet("https://raw.githubusercontent.com/ToRie-Ro/ZezerHubnotify/refs/heads/main/ZeZerHubNotify.lua"))()
+local ZeZerHubCh = loadstring(game:HttpGet("https://raw.githubusercontent.com/ToRie-Ro/Zezerscript/refs/heads/main/Checking.lua"))()
+local ZeZerHubNotify = loadstring(game:HttpGet("https://raw.githubusercontent.com/ToRie-Ro/Zezerscript/refs/heads/main/ZeZerHubNotify.lua"))()
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
