cd27eb6c292c44c48a644e9035697c3cc551666a
RealCambo <gamermakara40@gmail.com>
2025-05-15 11:52:27 +0700

    Update ZeZerHubNotify.lua

--- ZeZerHubNotify.lua
+++ ZeZerHubNotify.lua
@@ -2,7 +2,7 @@ local StarterGui = game:GetService("StarterGui")
 
 StarterGui:SetCore("SendNotification", {
     Title = "ZeZer Hub",
-    Text = "Remove workspace files?",
+    Text = "Successfully",
     Image = "rbxassetid://6031280882", -- Roblox friend icon
     Button1 = "Yes",
     Button2 = "No",
