af50525b240c8b1cdeeb4bcd5741e64dc3f76d62
RealCambo <gamermakara40@gmail.com>
2025-06-11 10:52:24 +0700

    Add files via upload

--- ZeZerHubNotify.lua
+++ ZeZerHubNotify.lua
@@ -1,8 +1,10 @@
 local StarterGui = game:GetService("StarterGui")
 
 StarterGui:SetCore("SendNotification", {
-    Title = "ZeZer Hub",
-    Text = "Successfully",
-    Image = "rbxassetid://6031280882", -- Roblox friend icon
+    Title = "MARU HUB",
+    Text = "Remove workspace files?",
+    Image = "rbxassetid://103221616248663",
+    Button1 = "Yes",
+    Button2 = "No",
     Duration = 10
 })
