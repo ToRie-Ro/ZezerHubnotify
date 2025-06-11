d682c641aa5d7cec5c5eca8001dab25aa8bb9f96
RealCambo <gamermakara40@gmail.com>
2025-06-11 10:53:19 +0700

    Update ZeZerHubNotify.lua

--- ZeZerHubNotify.lua
+++ ZeZerHubNotify.lua
@@ -1,10 +1,7 @@
 local StarterGui = game:GetService("StarterGui")
 
 StarterGui:SetCore("SendNotification", {
-    Title = "MARU HUB",
-    Text = "Remove workspace files?",
-    Image = "rbxassetid://103221616248663",
-    Button1 = "Yes",
-    Button2 = "No",
+    Title = "ZeZer Hub",
+    Text = "...",
     Duration = 10
 })
